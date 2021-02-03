4e0520742100e5d35c01b6d55c9117514582b085
AlfRagnar <69853212+Sharrior@users.noreply.github.com>
2021-02-03 18:38:23 +0100

    Fixed a bug where music player wouldn't close after finishing playing music

--- commands/play.js
+++ commands/play.js
@@ -146,50 +146,59 @@ module.exports = {
 		}
 	},
 
-	play(message, song) {
+	async play(message, song) {
 		const queue = message.client.queue;
 		const guild = message.guild.id;
 		const serverQueue = queue.get(guild);
 
-		if (!song) {
-			serverQueue.voiceChannel.leave();
-			queue.delete(guild);
-			return;
-		}
+		try {
+			if (!song) {
+				serverQueue.voiceChannel.leave();
+				queue.delete(guild);
+				return;
+			}
 
-		const dispatcher = serverQueue.connection
-			.play(ytdl(song.url))
-			.on('finish', () => {
-				serverQueue.songs.shift();
-				this.play(message, serverQueue.songs[0]);
-			})
-			.on('error', (error) => console.error(error));
+			const dispatcher = serverQueue.connection
+				.play(ytdl(song.url))
+				.on('finish', () => {
+					serverQueue.songs.shift();
+					this.play(message, serverQueue.songs[0]);
+				})
+				.on('error', (error) => console.error(error));
 
-		dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
-		// Create the message embed
-		const Author = song.author;
-		const AuthorThumbnail = song.author_thumbnail;
-		const nowPlaying = new Discord.MessageEmbed()
-			.setTitle(song.title)
-			.setURL(song.url)
-			.setAuthor(Author.name, AuthorThumbnail.url, Author.channel_url)
-			.addField('Song Length:', song.duration)
-			.setImage(song.image.url)
-			.setColor('#e67e22')
-			.setTimestamp();
-		// If song is a Livestream then change the text a little
-		if (song.duration <= 0) {
-			const receivedEmbed = message.embeds[0];
-			const liveEmbed = new Discord.MessageEmbed(receivedEmbed)
+			dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
+			// Create the message embed
+			const Author = song.author;
+			const AuthorThumbnail = song.author_thumbnail;
+			const nowPlaying = new Discord.MessageEmbed()
 				.setTitle(song.title)
 				.setURL(song.url)
 				.setAuthor(Author.name, AuthorThumbnail.url, Author.channel_url)
-				.setDescription('**LIVE**')
+				.addField('Song Length:', song.duration)
 				.setImage(song.image.url)
 				.setColor('#e67e22')
 				.setTimestamp();
-			return message.channel.send(liveEmbed);
+			// If song is a Livestream then change the text a little
+			if (song.duration <= 0) {
+				const receivedEmbed = message.embeds[0];
+				const liveEmbed = new Discord.MessageEmbed(receivedEmbed)
+					.setTitle(song.title)
+					.setURL(song.url)
+					.setAuthor(Author.name, AuthorThumbnail.url, Author.channel_url)
+					.setDescription('**LIVE**')
+					.setImage(song.image.url)
+					.setColor('#e67e22')
+					.setTimestamp();
+				return message.channel.send(liveEmbed);
+			}
+			return serverQueue.textChannel.send(nowPlaying);
+		} catch (error) {
+			console.error(error);
+			message.client.queue.delete(message.guild.id);
+			await channel.leave();
+			return message.channel
+				.send('Something went wrong, terminating music player')
+				.catch(console.error);
 		}
-		return serverQueue.textChannel.send(nowPlaying);
 	},
 };
