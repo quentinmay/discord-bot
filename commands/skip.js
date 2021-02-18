7e33f92a64e0471c7d462898ed270242bba4cde1
AlfRagnar <69853212+Sharrior@users.noreply.github.com>
2021-02-18 12:16:10 +0100

    Bug Fix: Skip command

--- commands/skip.js
+++ commands/skip.js
@@ -1,16 +1,20 @@
 module.exports = {
-	name: 'skip',
-	aliases: ['skipsong'],
-	description: 'Skip Request',
-	guildOnly: true,
-	usage: 'just type !skip',
-	execute(message) {
-		const queue = message.client.queue.get(message.guild.id);
-		const channel = message.channel;
-		if (!message.member.voice.channel)
-			return channel.send('You have to be in a voice channel to stop the music!');
-		if (!queue) return channel.send('There is no song that I could skip!');
-		channel.send(`Skipping: **${queue.songs[0].title}**`);
-		queue.connection.dispatcher.end();
-	},
+  name: 'skip',
+  aliases: ['skipsong'],
+  description: 'Skip Request',
+  guildOnly: true,
+  usage: 'just type !skip',
+  execute(message) {
+    const queue = message.client.queue.get(message.guild.id);
+    const channel = message.channel;
+    if (!message.member.voice.channel)
+      return channel.send(
+        'You have to be in a voice channel to stop the music!'
+      );
+    if (!queue) return channel.send('There is no song that I could skip!');
+    channel.send(`Skipping: **${queue.songs[0].title}**`);
+    queue.connection.dispatcher.end();
+
+    return channel.send('There is no queue or song to skip');
+  },
 };
